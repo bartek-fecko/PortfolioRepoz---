5e85b571029efabfac7cfcc0350100e4ef3fff72
bartek <bartlomiej.fecko@gmail.com>
2020-08-01 13:25:43 +0200

    first structure

---
+++ webpack/webpack.common.js
@@ -0,0 +1,72 @@
+const Path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: Path.resolve(__dirname, '../src/scripts/index.ts')
+  },
+  output: {
+    path: Path.join(__dirname, '../build'),
+    filename: 'js/[name].js'
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      name: false
+    }
+  },
+  plugins: [
+    new CleanWebpackPlugin(['build'], { root: Path.resolve(__dirname, '..') }),
+    new CopyWebpackPlugin([
+      { from: Path.resolve(__dirname, '../public'), to: 'public' }
+    ]),
+    new HtmlWebpackPlugin({
+      template: Path.resolve(__dirname, '../src/index.html')
+    })
+  ],
+  resolve: {
+    alias: {
+      '~': Path.resolve(__dirname, '../src')
+    },
+    extensions: [".ts", ".tsx", ".js"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        enforce: 'pre',
+        exclude: /node_modules/,
+        loader: 'tslint-loader'
+      },
+      {
+        test: /\.(js)$/,
+        enforce: 'pre',
+        include: Path.resolve(__dirname, '../src'),
+        loader: 'eslint-loader',
+        options: {
+          emitWarning: true,
+        }
+      },
+      {
+        test: /\.tsx?$/,
+        loaders: ['babel-loader', 'ts-loader'],
+      },
+      {
+        test: /\.mjs$/,
+        include: /node_modules/,
+        type: 'javascript/auto'
+      },
+      {
+        test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)(\?.*)?$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[path][name].[ext]'
+          }
+        }
+      },
+    ]
+  }
+};
